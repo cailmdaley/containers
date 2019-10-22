1adb0850f17ea99566f1c15a9990b43580445ab7
Cail Daley <cailmd2@illinois.edu>
2019-10-22 17:12:22 -0500

    preparing for merge

--- singularity.sh
+++ singularity.sh
@@ -4,6 +4,6 @@ SIF="$IMAGENAME"_latest
 
 INSTANCE=instance://$NAME
 
-# singularity pull docker://cailmdaley/$IMAGENAME
+echo singularity pull docker://cailmdaley/$IMAGENAME
 echo singularity instance start "$IMAGENAME"_latest.sif $INSTANCENAME
-singularity instance start "$IMAGENAME"_latest.sif $INSTANCENAME
+# singularity instance start "$IMAGENAME"_latest.sif $INSTANCENAME
