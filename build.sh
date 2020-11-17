c4ad9ea6ed7e6965d33382903e037344ae562c37
Cail Daley <cailmdaley@Cails-MBP.lan>
2020-11-17 17:11:26 -0600

    added a bunch of changes

--- build.sh
+++ build.sh
@@ -1,8 +1,21 @@
-docker build -t cailmdaley/coscine:v0.0.1 coscine
-docker push cailmdaley/coscine:v0.0.1 
+#/bin/bash
 
-docker build -t cailmdaley/coscine-tweaked:v0.0.1 coscine-tweaked
-docker push cailmdaley/coscine-tweaked:v0.0.1 
+[ -z $1 ] && echo "Please provide version tag" && exit
+COSCINE_VERSION=$1
 
-docker build -t cailmdaley/sptlab:v0.0.1 sptlab
-docker push cailmdaley/sptlab:v0.0.1 
+docker build \
+    -t cailmdaley/coscine:$COSCINE_VERSION \
+    -t cailmdaley/coscine:latest \
+    coscine
+
+docker build \
+    -t cailmdaley/coscine-tweaked:$COSCINE_VERSION \
+    -t cailmdaley/coscine-tweaked:latest \
+    --build-arg COSCINE_VERSION \
+    coscine-tweaked
+
+docker build \
+    -t cailmdaley/sptlab:$COSCINE_VERSION \
+    -t cailmdaley/sptlab:latest \
+    --build-arg COSCINE_VERSION \
+    sptlab
--- coscine-tweaked/jupyter_notebook_config.py
+++ coscine-tweaked/jupyter_notebook_config.py
@@ -2,3 +2,4 @@
 c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.notebook_dir = '/'
 c.ContentsManager.allow_hidden = True
+
